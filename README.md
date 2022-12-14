d9f6b7f1dd7bffb1d6634621939cc7cc17a77a00
tsja2001 <52786405+tsja2001@users.noreply.github.com>
2022-12-14 14:30:18 +0800

    add:完成初次提交:1/1

---
+++ README.md
@@ -0,0 +1,31 @@
+# webpack 打包完成后,自动远程部署插件
+
+# 安装
+```js
+npm i webpack-auto-upload-plugin -D
+```
+
+# 使用
+```js
+// webpack.config.js
+const WebpackAutoUploadPlugin = require('webpack-auto-upload-plugin')
+
+module.exports = {
+	...
+	plugins: [
+		new WebpackAutoUploadPlugin({
+			// 服务器地址(必传)
+			host: '123.456.789'
+			// 服务器用户名(默认root)
+			username: 'root'
+			// 服务器连接密码(必传)
+			password: 'xxxxxx'
+			// 文件部署到服务器的位置(必传)
+			remotePath: '/root/public'
+		})
+	]
+}
+```
+
+# 源码
+https://github.com/tsja2001/webpack-auto-upload-plugin
---
+++ webpack-auto-upload-plugin.js
@@ -0,0 +1,84 @@
+const { NodeSSH } = require('node-ssh')
+
+class MyPlugin {
+  constructor(options) {
+    this.ssh = new NodeSSH()
+    this.options = options
+    this.verifyOptions()
+  }
+  apply(compiler) {
+    compiler.hooks.afterEmit.tapAsync(
+      'AutoPlugin',
+      async (compilation, callback) => {
+        // 获取打包后的文件路径
+        const outputPath = compilation.outputOptions.path
+        // 远程服务器中文件的路径
+        const remotePath = this.options.remotePath + '/'
+
+        // ssh链接远程服务器
+        await this.connectServer()
+
+        // 先删除原有文件
+        await this.ssh.execCommand(`rm -rf ${remotePath}*`)
+
+        // 上传文件
+        await this.uploadFile(outputPath, remotePath)
+
+        // 关闭连接
+        await this.ssh.dispose()
+
+        callback()
+      }
+    )
+  }
+
+  // 连接服务器
+  async connectServer() {
+    console.log('正在连接远程服务器')
+
+    try {
+      await this.ssh.connect({
+        host: this.options.host,
+        username: this.options.username ?? 'root',
+        password: this.options.password,
+      })
+    } catch (error) {
+      throw new Error('服务器连接失败', error)
+    }
+
+    console.log('服务器连接成功')
+  }
+
+  // 上传文件
+  async uploadFile(loaclPath, remotePath) {
+    const status = await this.ssh.putDirectory(
+      loaclPath,
+      remotePath,
+      {
+        recursive: true, // 递归文件夹
+        concurrency: 10, // 并发数
+      }
+    )
+    if (status) {
+      console.log('上传成功')
+    } else {
+      console.log('文件上传失败')
+    }
+  }
+
+  // 验证传入options
+  async verifyOptions() {
+    if (!this.options.host) {
+      throw new Error('缺少host参数, 需要远程服务器地址')
+    }
+    if (!this.options.password) {
+      throw new Error('缺少password参数, 需要服务器密码')
+    }
+    if (!this.options.remotePath) {
+      throw new Error('缺少remotePath参数, 需要放远程文件存放路径')
+    }
+  }
+}
+
+module.exports = MyPlugin
+module.exports.MyPlugin = MyPlugin
